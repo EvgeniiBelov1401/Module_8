7699c0121f4fc96147397e227686372b2205fbc9
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 19:26:39 +0300

    Добавил Exercise_8_3_01

---
+++ Module_8/Exercise_8_3_01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_8_3_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Programming\Skillfactory\C#_projects\Module_8\Module_8\Exercise_8_3_01\Program.cs";
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string str = "";
+                while ((str=sr.ReadLine())!=null)
+                {
+                    Console.WriteLine(str); 
+                }
+            }
+        }
+    }
+}
