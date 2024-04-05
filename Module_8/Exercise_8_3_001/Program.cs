3603903abb1782170aeca30aea016ab82db372bc
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 19:20:24 +0300

    test

---
+++ Module_8/Exercise_8_3_001/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise_8_3_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Programming\Skillfactory\C#_projects\Module_8\FolderForFiles\Students.txt";
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter sw = File.CreateText(filePath))
+                {
+                    sw.WriteLine("Олег");
+                    sw.WriteLine("Иван");
+                    sw.WriteLine("Егор");
+                }
+            }
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string str = "";
+                while ((str=sr.ReadLine())!=null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
