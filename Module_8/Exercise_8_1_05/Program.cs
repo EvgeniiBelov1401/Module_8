9c64b89197cc2fc987ec1ee5120fdfd9152b4f5d
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 14:01:02 +0300

    Добавил Exercise_8_1_05

---
+++ Module_8/Exercise_8_1_05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_8_1_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
