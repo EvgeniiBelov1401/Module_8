9fb4719333cf674d5ab22d5dd680b92ec249bff5
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 15:38:52 +0300

    Exercise 8_2_3

--- Module_8/Exercise_8_2_01/Program.cs
+++ Module_8/Exercise_8_2_01/Program.cs
@@ -26,7 +26,19 @@
                 Console.WriteLine($"Кол-во папок: {direct.Length}\n" +
                     $"Кол-во файлов: {files.Length}\n");
             }
-            
+
+            string newDirNameForDelete = "D:\\NewFolder";
+            if (Directory.Exists(newDirNameForDelete))
+            {
+                Directory.Delete(newDirNameForDelete);
+                string[] direct = Directory.GetDirectories(dirName);
+                string[] files = Directory.GetFiles(dirName);
+
+                Console.WriteLine($"Кол-во папок: {direct.Length}\n" +
+                    $"Кол-во файлов: {files.Length}\n");
+            }
+
+
 
         }
     }
