1a550d16aec8eedf13b4b1d634fbaaaa1c0dba8c
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 16:54:08 +0300

    Добавил Exercise_8_2_04

---
+++ Module_8/Exercise_8_2_04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise_8_2_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\evgen\OneDrive\Рабочий стол\testFolder");
+            string newPath= @"C:\Users\evgen\OneDrive\Рабочий стол\NewFolder\testFolder";
+            if (dirInfo.Exists && !Directory.Exists(newPath)) 
+            {
+                dirInfo.MoveTo(newPath);
+            }
+        }
+    }
+}
