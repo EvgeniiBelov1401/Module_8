94f01362853f1bde0bbc2957a8bcb2e51829e530
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 13:53:03 +0300

    Добавил Exercise_8_1_04

---
+++ Module_8/Exercise_8_1_04/Modules/Drive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8_1_04.Modules
+{
+    internal class Drive
+    {
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+
+        public Drive(string name, long totalSpace,long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+    }
+}
---
+++ Module_8/Exercise_8_1_04/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercise_8_1_04.Modules;
+
+namespace Exercise_8_1_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drive drive = new Drive();
+        }
+    }
+}
