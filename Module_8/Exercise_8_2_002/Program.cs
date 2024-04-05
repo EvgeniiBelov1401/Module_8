03a1d8906bca3205d5b963fb835e79e3a1644fd8
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 15:11:17 +0300

    test

---
+++ Module_8/Exercise_8_2_002/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise_8_2_002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           GetCatalogs();
+        }
+        static void GetCatalogs()
+        {
+            string dirName = "D:\\";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Папки: ");
+                string[]dirs = Directory.GetDirectories(dirName);
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }    
+                    Console.WriteLine();
+                    Console.WriteLine("Файлы: ");
+                string[]files= Directory.GetFiles(dirName);
+                foreach(string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+    }
+}
