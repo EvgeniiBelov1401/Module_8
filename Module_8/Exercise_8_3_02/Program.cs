df1d58293efa95df482dc44e791bf5859e816698
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-05 19:52:33 +0300

    Добавил Exercise_8_3_02

---
+++ Module_8/Exercise_8_3_02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise_8_3_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Programming\Skillfactory\C#_projects\Module_8\Module_8\Exercise_8_3_02\Program.cs";
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            using (StreamWriter sw = fileInfo.AppendText())
+            {
+                sw.WriteLine($"//Время запуска: {DateTime.Now}");
+            }
+
+            using (StreamReader sr = fileInfo.OpenText())
+            {
+                string str = "";
+                while ((str = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+            //Console.WriteLine(fileInfo.LastAccessTime);
+        }
+    }
+}
+//Время запуска: 05.04.2024 19:51:37
+//Время запуска: 05.04.2024 19:52:04
