1e4da6e49b9dc6ef325d33e7d7fb99ffb51182f6
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-06 14:54:43 +0300

    Добавил Exercise_8_4_01

---
+++ Module_8/Exercise_8_4_01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise_8_4_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string StringValue;
+            string FileName = @"D:\Programming\Skillfactory\C#_projects\Module_8\FolderForFiles\BinaryFile.bin";
+            
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(FileName, FileMode.Open)))
+            {
+                writer.Write($"Дата создания: {DateTime.Now} Операционная система: {Environment.OSVersion}");
+            }
+            if (File.Exists(FileName))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(FileName, FileMode.Open)))
+                {
+                    StringValue = reader.ReadString();
+                }
+
+                Console.WriteLine($"Инфо: {StringValue}");
+            }
+        }
+    }
+}
