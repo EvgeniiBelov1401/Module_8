b787eb8b119b72406310443b9fd4d45c428f79fa
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-06 16:23:00 +0300

    Добавил Exercise_8_4_03

---
+++ Module_8/Exercise_8_4_03/Modules/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Exercise_8_4_03.Modules
+{
+    [Serializable]
+    internal class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name, long phoneNumber,string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
---
+++ Module_8/Exercise_8_4_03/Program.cs
@@ -0,0 +1,21 @@
+﻿using Exercise_8_4_03.Modules;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Exercise_8_4_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var newContact = new Contact("Иван", 89001112233, "ivan@mail.ru");
+            Console.WriteLine("Объект создан");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var fs=new FileStream(@"D:\Programming\Skillfactory\C#_projects\Module_8\FolderForFiles\Contact.dat",FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, newContact);
+                Console.WriteLine("Объект сериализован");
+            }
+        }
+    }
+}
